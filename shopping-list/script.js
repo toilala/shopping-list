f82e61bff5757706dbe3ee80e04b01ee09705e30
toilala <visitme.nk@gmail.com>
2025-01-10 23:03:52 +0545

    Added some clearUI compoents for making it more dynamic

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -1,6 +1,9 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
+const items = itemList.querySelectorAll("li");
+const itemFilter = document.getElementById("filter");
 
 function addItem(e) {
   e.preventDefault();
@@ -19,6 +22,7 @@ function addItem(e) {
   li.appendChild(button);
   itemList.appendChild(li);
   itemInput.value = "";
+  checkUI();
 }
 function createButton(classes) {
   const button = document.createElement("button");
@@ -33,6 +37,35 @@ function createIcon(classes) {
   icon.className = classes;
   return icon;
 }
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item"))
+    if (confirm("Are you sure?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  checkUI();
+}
+
+function clearItem() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+  checkUI();
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+}
 
 //Event Listeners
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItem);
+
+checkUI();
